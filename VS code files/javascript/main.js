f9d69b3aa39aa0a6d0241015bd0831ceac136588
sajeewa garusinghe <sajeeva616@gmail.com>
2022-05-20 01:00:36 +0530

    init commit
    this is initial commit of m common vs code files and pdfs except xampp files

---
+++ VS code files/javascript/main.js
@@ -0,0 +1,10 @@
+let s='tech,computer,it,coding';
+console.log('we');
+console.log(s.split(','));
+
+const ar=new Array(1,2,3,4);
+console.log(ar);
+const e=['we','you'];
+console.log(e);
+e.push('they');
+console.log(e);

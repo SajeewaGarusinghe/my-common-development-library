0763f575f018d9bf36a1d07796f317bb4e442bf0
sajeewa garusinghe <sajeeva616@gmail.com>
2022-05-29 20:59:54 +0530

    2022_05_29

---
+++ VS code files/js/Dom manipulation/dom.js
@@ -0,0 +1,23 @@
+// DISPLAY AN ALERT
+// alert('this is alert');
+//EXAMINE THE DOCUMENT OBJECT
+//console.dir(document);
+// console.dir(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+//change the document title
+//document.title="sajeewa";
+// console.log(document.doctype);
+// console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[7]);
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+//GETELEMENTBYID//
+console.log(document.getElementById('nav-text'));
+var navtitle=document.getElementById('nav-text');
+// navtitle.textContent='HELLOW';
+// navtitle.innerText='GOODBYE';
---
+++ VS code files/npm/index.js
@@ -0,0 +1,7 @@
+const _=require('lodash');
+
+const numbers=[33,45,67,34,22,56];
+_.each(numbers,function(number,i){
+console.log(number);
+});
+

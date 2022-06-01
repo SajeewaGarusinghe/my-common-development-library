ac6c5c24d915468b325201e9aafb4b5dc5c9b591
sajeewa garusinghe <sajeeva616@gmail.com>
2022-06-01 17:08:21 +0530

    done

--- VS code files/react js/hellowworld/src/App.js
+++ VS code files/react js/hellowworld/src/App.js
@@ -2,9 +2,13 @@
 import './App.css';
 
 function App() {
+const name='sajeewa';
+const x=100;
+
   return (
-    <div className="App">
-     <h1>Hellow world</h1>
+    <div className="Container">
+     <h1>Hellow {name}</h1>
+     <h2>Hellow{x<10 ? 'yes':'no'} </h2>
     </div>
   );
 }
